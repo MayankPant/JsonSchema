84434bf48caa886b9e8153d5fd7886ffd691ffca
Mayank-Affinsys <mayank.pant@affinsys.com>
2024-01-16 16:31:10 +0530

    Added the logic for validation

--- validateDemo.py
+++ validateDemo.py
@@ -1,17 +1,43 @@
 import jsonschema
 import json
 def validate(schema, data):
-    ""
+    if-(json.is_valid(data)):
+        return False
+
+    try:
+        requirements = schema["required"]
+        for req in requirements:
+            if(data.contains(req)):
+                val = data[req]
+                if -(type(val) == schema[req]["type"]):
+                    return False
+            else:
+                return True
+    except Exception as e:
+        print(e)
+        return False
+
+    
+        
+        
 
 
 
-user_file = input()
+user_file = {
+    "id": 1,
+    "name": "John Doe",
+    "age": 30,
+    "email": "john.doe@example.com"
+  }
 
-openFile = open("data/json_File.json", 'r')
+openFile = open("data/jsonFile.json", 'r')
 
 schema = json.loads(openFile.read())
 
-validate(schema, user_file)
+if(validate(schema, user_file)):
+    print("json is valid")
+else:
+    print("json not valid")
 
 
 
